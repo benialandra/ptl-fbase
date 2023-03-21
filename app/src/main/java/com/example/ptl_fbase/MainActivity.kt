ad711dab3da506bf191a488a61beaed3072a87e9
benialandra <benialandra13@gmail.com>
2023-03-21 15:39:02 +0700

    login session

--- app/src/main/java/com/example/ptl_fbase/MainActivity.kt
+++ app/src/main/java/com/example/ptl_fbase/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.ptl_fbase
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.UserHandle
+import com.example.ptl_fbase.adapter.ViewPagerAdapter
+import com.example.ptl_fbase.databinding.ActivityMainBinding
+import com.example.ptl_fbase.fragment.HomeFragment
+import com.example.ptl_fbase.fragment.UserFragment
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    setupTab()
     }
+
+    private fun setupTab() {
+        val adapter=ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(UserFragment(),"User")
+        binding.viewPager.adapter=adapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+        binding.tabs.getTabAt(0)!!.setIcon(R.drawable.ic_home)
+        binding.tabs.getTabAt(1)!!.setIcon(R.drawable.ic_persons)
+    }
+
+
 }
